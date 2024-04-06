661004ec7c066cc6b074d338edfa9f32d767990d
ADV Indian coder <vinaykumar860964@gmail.com>
2024-04-06 23:45:12 +0530

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Mentor
-* Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
-* Updated: Mar 19 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 
 (function() {
   "use strict";
